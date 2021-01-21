e2c0bfa71140886a7cfaa271dbda8337aca8a928
GrigoriyIL <ya.johncena2014@gmail.com>
2021-01-21 13:48:05 +0200

    initial commit

---
+++ git_hub_api/main_git_hub_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainGitHubApiConfig(AppConfig):
+    name = 'main_git_hub_api'
---
+++ git_hub_api/main_git_hub_api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+import requests as py_request
+
+
+def index(request):
+    pass
+    # response = py_request.get('https://api.github.com/repos/GrigoriyIL/-Decompilation/commits')
+    # result_2 = response.json()
+    # return HttpResponse(result_2[0]['commit'])
