078fc5c1e6a983ade9ecff99a64b36067a7d1601
GrigoriyIL <g.shubenko54@gmail.com>
2021-02-02 11:19:56 +0200

    [#3] changed the method of accessing models

--- git_hub_api/main_git_hub_api/views.py
+++ git_hub_api/main_git_hub_api/views.py
@@ -6,19 +6,19 @@ from datetime import datetime
 
 
 def index(request):
-
     return HttpResponse(get_repository(request))
 
 
 def get_repository(request):
     # Get models
     repository_users = RepositoryUser.objects.all()
-    repository = Repository.objects.all()
-    collaborator = Collaborator.objects.all()
+    repositorys = Repository.objects.all()
+    collaborators = Collaborator.objects.all()
     commits = Commit.objects.all()
+    full_repository_name = 'TelegramBot/Api'
 
     # Get repository URL
-    repository_url = "https://api.github.com/repos/GrigoriyIL/Git_hub_api"
+    repository_url = "https://api.github.com/repos/" + full_repository_name
     response_repository = py_request.get(repository_url).json()
     response_commits = py_request.get(repository_url + "/commits").json()
 
@@ -27,34 +27,42 @@ def get_repository(request):
 
     # Set information about users/collaborators
     if not repository_users.filter(name=login):
-        repository_users.create(name=login, is_github_user='True')
+        repository_users.create(name=login)
+    repository_user = repository_users.get(name=login)
 
     # Set information about repository
     date = datetime.strptime(response_repository['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-    if not repository.filter(repository_name=response_repository['name']):
-        repository.create(repository_name=response_repository['name'],
-                          owner=repository_users.get(name=login),
-                          date_of_creation=datetime.strptime(response_repository['created_at'], '%Y-%m-%dT%H:%M:%SZ'),
-                          repository_url=f'https://github.com/{response_repository["full_name"]}'
-                          )
+
+    if not repositorys.filter(repository_name=response_repository['name']):
+        repository_user.repository_set.create(
+            repository_name=response_repository['name'],
+            owner= repository_user.name,
+            date_of_creation= date,
+            repository_url=f'https://github.com/{response_repository["full_name"]}'
+        )
+
+
     for item in response_commits:
-        # way to committer name
         committer_name = item["commit"]["committer"]["name"]
-        committer_date = item["commit"]["committer"]["date"]
+        commit_date = item["commit"]["committer"]["date"]
         massage = item["commit"]["message"]
-        # Set collaborator
+
         if not repository_users.filter(name=committer_name):
             repository_users.create(name=committer_name)
+        repository_user = repository_users.get(name=committer_name)
+        
 
-        if not collaborator.filter(name=repository_users.get(name=committer_name).id):
-            collaborator.create(name=repository_users.get(name=committer_name),
-                                repository=repository.get(repository_name=response_repository['name']),
-                                is_github_user=repository_users.get(is_github_user='True')
-                                )
-        # Set commits
+        if not repository_user.collaborator.filter(name=repository_user.pk):
+            repository_user.collaborator.create(
+                name=committer_name, 
+                repository=repositorys.get(repository_name=response_repository['name']),
+                is_github_user =True
+            )
+        collab = collaborators.get(name_id=repository_user.pk)
+
         if not commits.filter(commit_massage=massage):
-            commits.create(committer=collaborator.get(name=repository_users.get(name=committer_name).id),
-                           commit_massage=massage,
-                           commit_date=datetime.strptime(committer_date, '%Y-%m-%dT%H:%M:%SZ')
-                           )
-
+            collab.commit_set.create(
+                committer=collab.pk,
+                commit_massage=massage,
+                commit_date=datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ')
+            )
