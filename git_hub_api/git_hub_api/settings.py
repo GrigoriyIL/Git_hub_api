8336f72c592f094469f4cff5a4b2b377a0dae0e7
GrigoriyIL <g.shubenko54@gmail.com>
2021-02-02 11:23:04 +0200

    automatic changes

--- git_hub_api/git_hub_api/settings.py
+++ git_hub_api/git_hub_api/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'main_git_hub_api')
 STATICFILES_DIR = {
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'main_hit_hub_api/static')
 }
